4c68be701a8e70fdb6a26872587aa0ba170c541e
Volker Adler <volker.adler@cern.ch>
2013-12-17 15:06:11 +0100

    Update MET filters.

--- TopQuarkAnalysis/Configuration/python/patRefSel_eventCleaning_cff.py
+++ TopQuarkAnalysis/Configuration/python/patRefSel_eventCleaning_cff.py
@@ -1,5 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
+# from RecoMET.METFilters.metFilters_cff import *
+
 from CommonTools.RecoAlgos.HBHENoiseFilter_cfi import *
 # s. https://hypernews.cern.ch/HyperNews/CMS/get/JetMET/1196.html
 HBHENoiseFilter.minIsolatedNoiseSumE        = 999999.
@@ -17,9 +19,13 @@ EcalDeadCellTriggerPrimitiveFilter.tpDigiCollection = cms.InputTag( 'ecalTPSkimN
 
 from RecoMET.METFilters.eeBadScFilter_cfi import *
 
+from RecoMET.METFilters.ecalLaserCorrFilter_cfi import *
+
 from RecoMET.METFilters.trackingFailureFilter_cfi import *
 
-from TopQuarkAnalysis.Configuration.patRefSel_eventCleaning_cfi import scrapingFilter
+from RecoMET.METFilters.trackingPOGFilters_cff import *
+
+from TopQuarkAnalysis.Configuration.patRefSel_eventCleaning_cfi import *
 
 eventCleaningData = cms.Sequence(
   scrapingFilter
@@ -28,11 +34,17 @@ eventCleaningData = cms.Sequence(
 eventCleaningMC = cms.Sequence(
 )
 
+# eventCleaning = cms.Sequence(
+#   metFilters
+# )
+
 eventCleaning = cms.Sequence(
   HBHENoiseFilter
 + CSCTightHaloFilter
 + hcalLaserEventFilter
 + EcalDeadCellTriggerPrimitiveFilter
 + eeBadScFilter
++ ecalLaserCorrFilter
 + trackingFailureFilter
++ trkPOGFilters
 )

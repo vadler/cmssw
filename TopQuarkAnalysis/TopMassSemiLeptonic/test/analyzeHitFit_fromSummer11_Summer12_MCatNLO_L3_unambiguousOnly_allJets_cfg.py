69d19e579f5f793fa27938ee55d86ccef0d3ac0d
Volker Adler <volker.adler@cern.ch>
2014-03-14 05:46:49 +0100

    Fix output path and add configs for different trigger settings.

--- TopQuarkAnalysis/TopMassSemiLeptonic/test/analyzeHitFit_fromSummer11_Summer12_MCatNLO_L3_unambiguousOnly_allJets_cfg.py
+++ TopQuarkAnalysis/TopMassSemiLeptonic/test/analyzeHitFit_fromSummer11_Summer12_MCatNLO_L3_unambiguousOnly_allJets_cfg.py
@@ -1237,7 +1237,7 @@ process.MessageLogger = cms.Service("MessageLogger",
 
 
 process.TFileService = cms.Service("TFileService",
-    fileName = cms.string('/afs/cern.ch/user/v/vadler/cms/Top/CMSSW_5_3_11/output/analyzeHitFit_fromSummer11_Summer12_MCatNLO_L3_unambiguousOnly_allJets.root')
+    fileName = cms.string('/afs/cern.ch/user/v/vadler/cms/Top/CMSSW_5_3_13/output/analyzeHitFit_fromSummer11_Summer12_MCatNLO_L3_unambiguousOnly_allJets.root')
 )
 
 

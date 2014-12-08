d36a17d359525b45f199f24c2099beac9318867d
Volker Adler <volker.adler@cern.ch>
2014-12-08 20:07:50 +0100

    Add comparisons for b-tag and lepton channels.

--- TopQuarkAnalysis/TopMassSemiLeptonic/bin/compareTopTransferFunctions_cfg.py
+++ TopQuarkAnalysis/TopMassSemiLeptonic/bin/compareTopTransferFunctions_cfg.py
@@ -205,6 +205,38 @@ compareTopTransferFunctions.compare = cms.PSet(
 ##, cycle    = cms.int32( 1 )
 ##)
 
+#label = 'MadGraph_leptonChannels'
+#compareTopTransferFunctions.compare.inputFiles.Default = cms.PSet( # "Default" *must* exist!
+  #fileName = cms.string( 'file:%s/output/fitTopTransferFunctions_fromSummer11-coarseEta_Summer12_MadGraph_L3_unambiguousOnly_PileUp.root'%( os.getenv( "CMSSW_BASE" ) ) )
+#, legendLabel = cms.string( 'both lepton channels' )
+#, useAlt     = cms.bool( False )
+#, useNonT    = cms.bool( True )
+#, useNonP    = cms.bool( True )
+#, refGen     = cms.bool( True )
+#, refSel     = cms.bool( True )
+#, cycle    = cms.int32( 1 )
+#)
+#compareTopTransferFunctions.compare.inputFiles.Compare1 = cms.PSet(
+  #fileName = cms.string( 'file:%s/output/fitTopTransferFunctions_fromSummer11-coarseEta_Summer12_MadGraph_L3_unambiguousOnly_elecChannel_PileUp.root'%( os.getenv( "CMSSW_BASE" ) ) )
+#, legendLabel = cms.string( 'electron channel only' )
+#, useAlt     = cms.bool( False )
+#, useNonT    = cms.bool( True )
+#, useNonP    = cms.bool( True )
+#, refGen     = cms.bool( True )
+#, refSel     = cms.bool( True )
+#, cycle    = cms.int32( 1 )
+#)
+#compareTopTransferFunctions.compare.inputFiles.Compare2 = cms.PSet(
+  #fileName = cms.string( 'file:%s/output/fitTopTransferFunctions_fromSummer11-coarseEta_Summer12_MadGraph_L3_unambiguousOnly_muonChannel_PileUp.root'%( os.getenv( "CMSSW_BASE" ) ) )
+#, legendLabel = cms.string( 'muon channel only' )
+#, useAlt     = cms.bool( False )
+#, useNonT    = cms.bool( True )
+#, useNonP    = cms.bool( True )
+#, refGen     = cms.bool( True )
+#, refSel     = cms.bool( True )
+#, cycle    = cms.int32( 1 )
+#)
+
 ### Paths ###
 
 logFile = 'compareTopTransferFunctions_%s.log'%( label )
